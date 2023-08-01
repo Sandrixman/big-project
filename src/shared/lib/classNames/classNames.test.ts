fda3713eef9899945a04f07050f16ca26d8d946b
Oleksander Novak <sandrix@ukr.net>
2023-08-01 16:29:19 +0300

    add jest and first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,22 @@
+import { classNames }  from './classNames';
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    expect(classNames('class')).toBe('class');
+  })
+
+  test('with additional class', () => {
+    const expected = 'class cl1 cl2'
+    expect(classNames('class', {}, ['cl1', 'cl2'])).toBe(expected);
+  })
+
+  test('with mods', () => {
+    const expected = 'class cl1 cl2 hovered'
+    expect(classNames('class', {hovered: true, checked: false}, ['cl1', 'cl2'])).toBe(expected);
+  })
+
+  test('with mods undefined', () => {
+    const expected = 'class cl1 cl2 hovered'
+    expect(classNames('class', {hovered: true, checked: undefined}, ['cl1', 'cl2'])).toBe(expected);
+  })
+})
