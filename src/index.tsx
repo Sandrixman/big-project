fc6ac5e225de4c91861a05755cca0a1ec4415187
Oleksander Novak <sandrix@ukr.net>
2023-08-03 12:07:35 +0300

    fixed eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { ErrorBoundary } from "app/providers/ErrorBoundary/ui/ErrorBoundary";
-import { ThemeProvider } from "app/providers/ThemeProvider";
-import App from "app/App";
-import "shared/config/i18n/i18n";
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary/ui/ErrorBoundary';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+import App from 'app/App';
+import 'shared/config/i18n/i18n';
 
 render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
+    </BrowserRouter>,
+    document.getElementById('root'),
 );
